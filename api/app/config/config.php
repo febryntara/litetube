178824f1e99613357e333f4211a055a08213bf8f
febryntara <paglutgede@gmail.com>
2022-01-25 13:05:13 +0800

    litetube v1

--- api/app/config/config.php
+++ api/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Base url
-const BASEURL = "http://localhost:8080/lotetube/api/public";
+const BASEURL = "https://litetube.vercel.app/api/public";
 
 // Database
 const DB_HOST = "localhost";
